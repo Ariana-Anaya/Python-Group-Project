d0d3a81cbe49d737cb082d31e6dd5dc221ce8386
wolf-domination <165001309+wolf-domination@users.noreply.github.com>
2025-07-21 15:54:42 -0500

    07/21-update

---
+++ app/models/business_image.py
@@ -0,0 +1,23 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+
+class BusinessImage(db.Model):
+    __tablename__ = 'business_images'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    business_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("businesses.id")), nullable=False)
+    url = db.Column(db.String(255), nullable=False)
+    preview = db.Column(db.Boolean, nullable=False, default=False)
+
+    # Relationships
+    business = db.relationship("Business", back_populates="business_images")
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'url': self.url,
+            'preview': self.preview
+        }
